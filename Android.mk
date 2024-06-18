c7e47365bb1e259175f79dbad913cdae0fafd903
rio004 <riyon336@gmail.com>
2024-06-19 00:57:07 +0530

    duchamp: Import Firmware
    Change-Id: I0739bb1c59317a572c8d6ca4b030d522043d2d76
    Signed-off-by: rio004 <riyon336@gmail.com>

--- Android.mk
+++ Android.mk
@@ -6,4 +6,23 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),duchamp)
 
+$(call add-radio-file-sha1-checked,radio/apusys.img,36ace7b5b0a0d9d092c7b081e189efa9d7c57835)
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,de2649db44fc00f301017e49f2fe9f1dc85ec0b2)
+$(call add-radio-file-sha1-checked,radio/ccu.img,426c60b3174174fa11df9121f4bd2d353d221cdb)
+$(call add-radio-file-sha1-checked,radio/connsys_bt.img,f1c5dedf02bc79f1bfac4de46591389b1bce909d)
+$(call add-radio-file-sha1-checked,radio/connsys_gnss.img,717a75221efa3ef7c2e980104214bbef79f3ffd4)
+$(call add-radio-file-sha1-checked,radio/connsys_wifi.img,6aa3b74480c9be7336da29aed0fc780d3b6a8370)
+$(call add-radio-file-sha1-checked,radio/dpm.img,ff6eaa346c1d81b68f7e3bb194f48571c3060324)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,6f88dee51c339cd2a2b973664897ad5a442d72cb)
+$(call add-radio-file-sha1-checked,radio/gz.img,bc0149fe22fb1b15d40e0a1f0990b36f3c36ed3c)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,0dbca210232597bfa55aa6c5c6ab3039e7aa6b2d)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,ca07a379a4c6a93a45f2a84b3514759331804586)
+$(call add-radio-file-sha1-checked,radio/mvpu_algo.img,8b9dd31a95e6d8502c847bda8209cdf87b6a5769)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,954d21dca51d125598aeca0fd35d39f165dd4c70)
+$(call add-radio-file-sha1-checked,radio/scp.img,13e7d6d0a3fac60346649e65233033c4e1e1e67b)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,b1661cead313175856619dd7f0e4ee5169596a48)
+$(call add-radio-file-sha1-checked,radio/sspm.img,8b439db2fb9376dab85fc7f896a37cf8f53e2d12)
+$(call add-radio-file-sha1-checked,radio/tee.img,015cac5b0afebf07b627566f96b7026ffa9836bb)
+$(call add-radio-file-sha1-checked,radio/vcp.img,523bd2eae455b53d5c3c875ed005c4d425a5ce15)
+
 endif
